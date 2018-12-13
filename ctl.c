9590836c2b27c266581113da79902a718c2c317d
StefanTan18 <stan3@stuy.edu>
2018-12-13 11:02:02 -0500

    updted ctl.c

--- ctl.c
+++ ctl.c
@@ -8,11 +8,18 @@
 
 #define SEM_KEY 1234
 
+union semun {
+  int val;
+  struct semid_ds *buf;
+  struct seminfo *_buf;
+}
+
 int ctl() {
   key_t key;
-  int shmid, fd;
+  int shmid, semd, fd;
   char *data;
   char r[200];
+  union semun arg;
   
   fgets(r, 200, stdin);
   if (!strcmp(r, "-c\n")) {
@@ -20,5 +27,16 @@ int ctl() {
     shmid = shmget(key, 200, 0644 | IPC_CREAT);
     data = shmat(shmid, (void *)0, 0);
 
+    semd = semget(SEM_KEY, 1, IPC_CREAT | IPC_EXCL );
+
     fd = open("story.txt", O_CREAT | O_TRUNC | O_EXCL);
+  }
+  
+  if (!strcmp(r, "-r\n")) {
+    shmdt(data);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    semctl(semd, 0, IPC_RMID, arg);
+
+    //excvp(cat)
   }
