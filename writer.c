065acb62b8fa1d40c514afaad1710daf348f0905
djiang2 <djiang2>
2018-12-13 11:01:28 -0500

    init2

--- writer.c
+++ writer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 #include <sys/ipc.h>
 #include <sys/types.h>
 #include <errno.h>
@@ -10,17 +11,32 @@ union semun{
   int val;
   struct semid_ds* buf;
   struct seminfo* _buf;
-}
+};
 
-void writer(int semdesc){
+
+void writer(int semdesc,key_t key){
 
   union semun arg;
+  struct sembuf buf;
   
   int semVal = semctl(semdesc,0,GETVAL,arg);
-  printf("Semctrl %d\n",semVal);
+  
+  if(semVal > 0){
+    buf.sem_num = 0;
+    buf.sem_op = -1;
+    buf.sem_flag = SEM_UNDO;
+
+    printf("Enter next line: \n");
+    fgets(
+	  );
+
+    buf.sem_num = 0;
+    buf.sem_op = +1;
+    buf.sem_flag = SEM_UNDO;
+  }
+  
+  printf("Semctrl %d %d %s\n",semVal,errno,strerror(errno));
+  return;
 }
 
-int main(){
-  writer(1);
-  return 0;
-}
+
