8a975de0e63e4c3269f47b2c204f9d8ddae115e7
Stefan Tan <stan3@stuy.edu>
2018-12-14 00:49:43 -0500

    updated files

--- makefile
+++ makefile
@@ -1,5 +1,12 @@
 all: ctl.o writer.o
-	gcc ctl.c writer.c
+	gcc -o ctl ctl.c
+	gcc -o writer writer.c
+
+ctl.o:
+	gcc -c ctl.c
+
+writer.o:
+	gcc -c writer.c
 
 clean:
 	rm *.o
--- writer.c
+++ writer.c
@@ -6,6 +6,8 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 union semun{
   int val;
@@ -14,39 +16,49 @@ union semun{
 };
 
 
-void writer(int semdesc,int size){
+int main(){
 
-  
-  key_t key = ftok("ctl.c", "R")
+  key_t key = ftok("ctl.c", 'R');
   union semun arg;
   struct sembuf buf;
   char line[250];
 
   int fd = open("story.txt", O_WRONLY|O_APPEND);
-  int shmid = shmget(key,size,0644);
-  int semVal = semctl(semdesc,0,GETVAL,arg);
+  int shmid = shmget(key, 200, 0644);
+  int semid = semget(key, 1, 0);
+  int semVal = semctl(semid,0,GETVAL,arg);
+
+  if (!semVal) {
+    printf("Segment is unavailable.\n");
+    return 0;
+  }
   
   buf.sem_num = 0;
   buf.sem_op = -1;
-  buf.sem_flag = SEM_UNDO;
+  buf.sem_flg = SEM_UNDO;
 
-  semop(semdesc,&buf,1);
+  semop(semid,&buf,1);
 
   char *prev_line = shmat(shmid,0,0);
-  printf("Previous line: %s \n", prev_line);
+  if (!*prev_line) {
+    printf("New Story!\n");
+  }
+  else {
+    printf("Previous line: %s \n", prev_line);
+  }
 
   printf("Enter next line: ");
   fgets(line,250,stdin);
   line[strlen(line) - 1] = '\0';
   strcpy(prev_line,line);
   write(fd,line,250);
-  clode(fd);
-
-  buf.sem_op = +1;
-  semop(semdesc,&buf,1);
+  close(fd);
+  
+  buf.sem_op = 1;
+  semop(semid,&buf,1);
   
   //printf("Semctrl %d %d %s\n",semVal,errno,strerror(errno));
-  return;
+  return 0;
 }
 
 
