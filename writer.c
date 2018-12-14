621a2fd9f0b2da6a34e2e318efcc2fa2ad1b6fe7
djiang2001 <djiang2@stuy.edu>
2018-12-13 23:37:09 -0500

    user error

--- writer.c
+++ writer.c
@@ -14,28 +14,36 @@ union semun{
 };
 
 
-void writer(int semdesc,key_t key){
+void writer(int semdesc,key_t key,int size){
 
   union semun arg;
   struct sembuf buf;
-  
+  char line[250];
+
+  int fd = open("story.txt", O_WRONLY|O_APPEND);
+  int shmid = shmget(key,size,0644);
   int semVal = semctl(semdesc,0,GETVAL,arg);
   
-  if(semVal > 0){
-    buf.sem_num = 0;
-    buf.sem_op = -1;
-    buf.sem_flag = SEM_UNDO;
+  buf.sem_num = 0;
+  buf.sem_op = -1;
+  buf.sem_flag = SEM_UNDO;
 
-    printf("Enter next line: \n");
-    fgets(
-	  );
+  semop(semdesc,&buf,1);
 
-    buf.sem_num = 0;
-    buf.sem_op = +1;
-    buf.sem_flag = SEM_UNDO;
-  }
+  char *prev_line = shmat(shmid,0,0);
+  printf("Previous line: %s \n", prev_line);
+
+  printf("Enter next line: ");
+  fgets(line,250,stdin);
+  line[strlen(line) - 1] = '\0';
+  strcpy(prev_line,line);
+  write(fd,line,250);
+  clode(fd);
+
+  buf.sem_op = +1;
+  semop(semdesc,&buf,1);
   
-  printf("Semctrl %d %d %s\n",semVal,errno,strerror(errno));
+  //printf("Semctrl %d %d %s\n",semVal,errno,strerror(errno));
   return;
 }
 
